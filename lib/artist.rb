1c7b0e84cf71eff03a0ffbe5fa2183677e37c9ab
Nina Kazzim <kazzim@icloud.com>
2019-08-03 00:18:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,6 @@
 class Artist
   attr_accessor :name, :songs
 
-  @@song_count = 0
-
   def initialize(name)
     @name = name
     @songs = []
@@ -15,17 +13,16 @@ class Artist
   def add_song(song)
     self.songs << song
     song.artist = self
-    @@song_count += 1
   end
 
   def add_song_by_name(song_name)
     song = Song.new(song_name)
     self.songs << song
     song.artist = self
-    @@song_count += 1
   end
 
   def self.song_count
-    @@song_count
+    
   end
+  
 end
